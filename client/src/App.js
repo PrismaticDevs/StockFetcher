20d7c34e366e588fd3f32e277c133a4d33e1f93c
Matthew Brignola <matthewbrignola@ymail.com>
2023-01-06 22:57:36 -0700

    starting building front end pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,21 @@ import Layout from "./Layout";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <ResponsiveAppBar />
+    </>
   );
 }
